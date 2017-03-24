b4110564c15623ba5df1178dc9b52148d2ae54ee
patrickpowers848@gmail.com <patrickpowers848@gmail.com>
2017-03-24 19:41:49 -0400

    added comments to app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,22 @@
-
+//Define the time you want to elapse before the timer runs out
 var time = 60;
 
 
 function startGame(){	
 
-		function reset() {
-
-		}
 		
 		function answers(){
-			
+			//Fairly self-explantory. If the right radio buttons are pressed, then you win the game.
+			//The right radio buttons are determined by their index number in the array of radio buttons.
 			if ($('input:radio[name="question1"]')[0].checked === true && $('input:radio[name="question2"]')[2].checked === true && $('input:radio[name="question3"]')[3].checked === true) {	
 
 				alert("you win");
+				//this refreshes the page.
 				location.reload();
+				//empties the radio buttons
 				$(":radio").prop("checked", false)
 			}
-
+			//If you don't press the right radio buttons, you lose.
 			else {
 				alert("you lose");
 				location.reload();
@@ -29,14 +29,17 @@ function startGame(){
 
 
 
-
+		//This sets the timer, and if time runs out, run the answers() function.
 		var setTime = setInterval(function() {
 			if (time === 0){
+				//clearInterval clears a timer set by setInterval
 				clearInterval(setTime);
 				answers();
 			}
+			//If the time hasn't run out, keep running down the clock
 			else{
 			time--;
+			//Display the timer to #seconds div
 			$('#seconds').html(time);}			
 		},1000);
 	
