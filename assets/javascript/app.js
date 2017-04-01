9699da68151eac173b87d4eb89ca72532db4fb22
patrickpowers848@gmail.com <patrickpowers848@gmail.com>
2017-04-01 00:45:42 -0400

    game now lists right/wrong answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,35 +3,37 @@ var time = 61;
 
 
 function startGame(){	
-
-		
-		$("#submitButton").on("click", function answers(){
+	
+	$("#submitButton").on("click", function(){
 			//Fairly self-explantory. If the right radio buttons are pressed, then you win the game.
 			//The right radio buttons are determined by their index number in the array of radio buttons.
 			
+			var answer1 = 'input:radio[name="question1"]';
+			var answer2 = 'input:radio[name="question2"]';
+			var answer3 = 'input:radio[name="question3"]';
 			
-			if ($('input:radio[name="question1"]')[0].checked === true && $('input:radio[name="question2"]')[2].checked === true && $('input:radio[name="question3"]')[3].checked === true) {	
+			if ($(answer1)[0].checked === true && $(answer2)[2].checked === true && $(answer3)[3].checked === true){	
+					alert("You got everything right!");
+			} 
+				else if($(answer1)[0].checked === true && $(answer2)[2].checked === true && $(answer3)[3].checked === false){
+					alert("You got 2 questions right!");
+	
+				}
+				else if($(answer1)[0].checked === true && $(answer2)[2].checked === false && (answer3)[3].checked === false){
+					alert("You got 1 question right!");
+					}
+				else if($(answer1)[0].checked === true && $(answer2)[2].checked === false && $(answer3)[3].checked === true){
+					alert("You got 2 questions right!");}
+				else if($(answer1)[0].checked === false && $(answer2)[2].checked === true && $(answer3)[3].checked === true){
+					alert("You got 2 questions right!");}
+				else if($(answer1)[0].checked === false && $(answer2)[2].checked === false && $(answer3)[3].checked === true ){
+					alert("You got 1 question right!");}
+					else{
+					alert("Sorry, you didn't get any questions right");
+				}
+			});
 
-				alert("you win");
-				//this refreshes the page.
-				location.reload();
-				//empties the radio buttons
-				$(":radio").prop("checked", false)
-			}
-			//If you don't press the right radio buttons, you lose.
-			else {
-				alert("you lose");
-				location.reload();
-				$(":radio").prop("checked", false)
-			}
-
-
-		
-		});
-
-
-
-		//This sets the timer, and if time runs out, run the answers() function.
+	//This sets the timer, and if time runs out, run the answers() function.
 		var setTime = setInterval(function() {
 			if (time === 0){
 				alert("you lose");
@@ -43,8 +45,9 @@ function startGame(){
 			//Display the timer to #seconds div
 			$('#seconds').html(time);}			
 		},1000);
-	
 }
+
+
 
 startGame();
 
